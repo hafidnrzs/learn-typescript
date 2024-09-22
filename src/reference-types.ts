8f4625c83c1d12fcc9b7055cab744462daec773b
hafidnrzs <hafidnurazis@gmail.com>
2024-09-22 17:30:07 +0700

    reference types
    pointer in Typescript

---
+++ src/reference-types.ts
@@ -0,0 +1,14 @@
+// Primitive types
+let x = 2;
+let y = 1;
+
+x = y;
+y = 100;
+console.log("Primitive type. x =", x);
+
+// Reference types
+let point1 = { x: 1, y: 1 };
+let point2 = point1;
+
+point1.y = 100;
+console.log("Reference type. y =", point2.y);
